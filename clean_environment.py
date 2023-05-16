26ed9a1f8035315451122a971181ba9423b2e480
dor-solomon <68469554+dor-solomon@users.noreply.github.com>
2023-05-16 22:19:16 +0300

    Jenkinsfile discard old build logs and add clean_environment

--- clean_environment.py
+++ clean_environment.py
@@ -1,6 +1,9 @@
 import requests
 try:
     requests.get('http://127.0.0.1:5000/stop_server')
-    requests.get('http://127.0.0.1:5000/stop_server')
 except:
-    pass
+    print("no response")
+try:
+    requests.get('http://127.0.0.1:5001/stop_server')
+except:
+    print("no response")
