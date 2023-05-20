7bd626fc985c23baa5f4ea23ce1e2f69dc2cd795
dor-solomon <68469554+dor-solomon@users.noreply.github.com>
2023-05-20 23:59:08 +0300

    Add error handling to the flask servers.

--- web_app.py
+++ web_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from db_connector import get_user, cmd_args
 import os
 import signal
@@ -7,6 +7,29 @@ from sys import argv
 cmd_args(argv)
 
 app = Flask(__name__)
+
+
+class InvalidUsage(Exception):
+    status_code = 404
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+
+@app.errorhandler(InvalidUsage)
+def handle_error(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 
 @app.route('/users/get_user_data/<user_id>', methods=['GET'])
