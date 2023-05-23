b275cb3ab5bd80848998b4df7f9fc4644e846e3d
dor-solomon <68469554+dor-solomon@users.noreply.github.com>
2023-05-23 14:13:33 +0300

    Remove duplication

--- frontend_testing.py
+++ frontend_testing.py
@@ -34,16 +34,7 @@ for option in options:
     chrome_options.add_argument(option)
 
 firefox_options = Optionsf()
-optionsf = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
-]
-for option in optionsf:
+for option in options:
     firefox_options.add_argument(option)
 
 try:
