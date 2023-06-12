585e7a3dc36e8a02b6b730a6bc2f0280e28afab7
dor-solomon <68469554+dor-solomon@users.noreply.github.com>
2023-06-12 22:05:47 +0300

    Add docker_backend_testing.py

---
+++ docker_backend_testing.py
@@ -0,0 +1,20 @@
+import requests
+from db_connector import config, cmd_args
+from sys import argv
+
+cmd_args(argv)
+user_id = 4
+config = config()
+user_name = config[2]
+link = config[0]
+
+try:
+    requests.post(f'{link}{user_id}', json={"user_name":f"{user_name}"})
+except:
+    raise Exception("test failed")
+res = requests.get(f'{link}{user_id}')
+resp = res.json()
+if resp.get("user_name") == f"{user_name}" and res.status_code == 200:
+    print(resp.get("user_name"), res.status_code)
+else:
+    raise Exception("test failed")
